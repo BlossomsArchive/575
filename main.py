27ca604b5ba37effb7a914c8dd8349703f4e0798
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-09-26 20:12:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ import random
 import os
 import time
 
-misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
-misskey_token = os.environ.get("MISSKEY_TOKEN")
-
-senryu = Misskey(misskey_address)
-senryu.token = misskey_token
-
 #使える文字のリスト
 hiragana = ['ぁ', 'あ', 'ぃ', 'い', 'ぅ', 'う', 'ぇ', 'え', 'ぉ', 'お', 'か', 'が', 'き', 'ぎ', 'く', 'ぐ', 'け', 'げ', 'こ', 'ご', 'さ', 'ざ', 'し', 'じ', 'す', 'ず', 'せ', 'ぜ', 'そ', 'ぞ', 'た', 'だ', 'ち', 'ぢ', 'っ', 'つ', 'づ', 'て', 'で', 'と', 'ど', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ば', 'ぱ', 'ひ', 'び', 'ぴ', 'ふ', 'ぶ', 'ぷ', 'へ', 'べ', 'ぺ', 'ほ', 'ぼ', 'ぽ', 'ま', 'み', 'む', 'め', 'も', 'ゃ', 'や', 'ゅ', 'ゆ', 'ょ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'ゎ', 'わ', 'ゐ', 'ゑ', 'を', 'ん','ー']
 
@@ -46,6 +40,10 @@ while True:
 #できた川柳
 while True:
     try:
+        misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
+        misskey_token = os.environ.get("MISSKEY_TOKEN")
+        senryu = Misskey(misskey_address)
+        senryu.token = misskey_token
         senryu.notes_create(text=syoku+"\n"+niku+"\n"+kekku)
     except:
         time.sleep(300)
